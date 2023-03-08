64dc7bfc65b8dfe7b7d12d92eec91ec1aca8d637
Lukasz <bakatjur@gmail.com>
2023-03-08 09:30:00 +0000

    Add responsivness for 320px width mobile devices, landscape mobile mode up to 767px and add function hide the countdown-date class div after push start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,46 @@
-// set the countdown date and time (YYYY-MM-DD HH:MM:SS format)
-const countdownDate = new Date('2023-03-08T07:59:59').getTime();
+function startCountdown() {
+    // get the countdown date and time from the input element
+    const countdownInput = document.getElementById('countdown-input').value;
+    // set the countdown date and time (YYYY-MM-DD HH:MM:SS format)
+    const countdownDate = new Date(countdownInput).getTime();
 
-// get the timer elements from the HTML
-const daysEl = document.querySelector('.days .time');
-const hoursEl = document.querySelector('.hours .time');
-const minutesEl = document.querySelector('.minutes .time');
-const secondsEl = document.querySelector('.seconds .time');
+    // hide the div with class countdown-date
+    const countdownDateEl = document.querySelector('.countdown-date');
+    countdownDateEl.style.display = 'none';
 
-const countdown = setInterval(() => {
-        // get the current date and time
-        const now = new Date().getTime();
+    // get the timer elements from the HTML
+    const daysEl = document.querySelector('.days .time');
+    const hoursEl = document.querySelector('.hours .time');
+    const minutesEl = document.querySelector('.minutes .time');
+    const secondsEl = document.querySelector('.seconds .time');
 
-        // caklculate the time remaining
-        const timeRemaining = countdownDate - now;
+    const countdown = setInterval(() => {
+            // get the current date and time
+            const now = new Date().getTime();
 
-        // calculate days, hours, minutes and seconds remaining
-        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+            // caklculate the time remaining
+            const timeRemaining = countdownDate - now;
 
-        // update the timer display
-        daysEl.innerHTML = days < 10 ? `0${days}` : days;
-        hoursEl.innerHTML = hours < 10 ? `0${hours}` : hours;
-        minutesEl.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-        secondsEl.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+            // calculate days, hours, minutes and seconds remaining
+            const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-        // check if the countdown has ended
-        if (timeRemaining < 0) {
-            clearInterval(countdown);
-            daysEl.innerHTML = '00';
-            hoursEl.innerHTML = '00';
-            minutesEl.innerHTML = '00';
-            secondsEl.innerHTML = '00';
-        }
-    },
-    1000);
+            // update the timer display
+            daysEl.innerHTML = days < 10 ? `0${days}` : days;
+            hoursEl.innerHTML = hours < 10 ? `0${hours}` : hours;
+            minutesEl.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+            secondsEl.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+            // check if the countdown has ended
+            if (timeRemaining < 0) {
+                clearInterval(countdown);
+                daysEl.innerHTML = '00';
+                hoursEl.innerHTML = '00';
+                minutesEl.innerHTML = '00';
+                secondsEl.innerHTML = '00';
+            }
+        },
+        1000);
+}
